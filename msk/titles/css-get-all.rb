c0a0f61a4774ffb3cec78103de8de5e673435feb
Hermes Passer <dsldouglas@hotmail.com>
2023-01-28 19:14:18 -0300

    update Countless Swords Sorcerer script to work on the redesined site

--- msk/titles/css-get-all.rb
+++ msk/titles/css-get-all.rb
@@ -2,8 +2,8 @@ require_relative '../shared'
 
 def get_chapter_text url
     content = get_html_document url
-    content = content.xpath('//div[@class="entry-content"]')[0]
-    content.search('script').remove
+    # there are two <main> elements, so we get by the class
+    content = content.xpath("//main[contains(@class, 'roboto-serif')]")
     content
 end
 
@@ -13,10 +13,17 @@ def download_all
     file << '<h1>Countless Swords Sorcerer</h1> <p>translated by <a href="www.miraslation.net">Mira\'s Cocktail</a></p>'   
     
     # prev chs by other group
-    first_release = 16
-    last_released = 262
-    (first_release..last_released).each do |i|
-        url = "https://www.miraslation.net/novels/css/" + i.to_s.rjust(4, '0')
+    first_release = 13
+    last_release = 335
+    (first_release..last_release).each do |i|
+	base_url = 'https://www.miraslation.net'
+	# after the redesing they altered the urls
+	if i < 334
+		# this redirects to https://www.miraslation.net/novels/css/chapters/<i>/0<i>/
+		url = base_url + "/novels/css/" + i.to_s.rjust(4, '0')
+	else
+		url = base_url + "/novels/css/chapters/" + i.to_s + "/countless-swords-sorcerer-%E2%80%95%E2%80%95-chapter-" + i.to_s
+	end        
         
         puts "Downloading ch #{i} -> #{url}"
         file << "<h2>Chapter #{i}</h2>"
